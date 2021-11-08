7e1fa8c313f0136f4c65365f6c482daeb8624e9b
tomas-rosak <93378161+tomas-rosak@users.noreply.github.com>
2021-11-08 09:04:34 +0100

    Update black_jack.py

--- black_jack.py
+++ black_jack.py
@@ -3,8 +3,8 @@ import random
 import os
 pokracovat_program = True
 print("Black Jack\n")
-name_hrac1 = input("Hrac 1 prezdivka: ")
-name_hrac2 = input("Hrac 2 prezdivka: ")
+name_hrac1 = input("Hráč 1 přezdívka: ")
+name_hrac2 = input("Hrac 2 přezdívka: ")
 while pokracovat_program:
     pouzite_karty = list()
     blacklist = list()
@@ -14,12 +14,64 @@ while pokracovat_program:
     soucet2 = 0
     hrac1 = True
     hrac2 = False
-    pokracovat_hra = True
+    prvni_kolo = True
+    spravna_odpoved1 = True
+    spravna_odpoved2 = True
+    spravna_odpoved3 = True
+    spravna_odpoved4 = True
+    spravna_odpoved5 = True
+    spravna_odpoved6 = True
+    spravna_odpoved7 = True
+    pokracovat_hra = False
+    while prvni_kolo:
+        input(name_hrac1 + ": Stiskněte ENTER k vytáhnutí karty: ")
+        karta1 = random.randint(1, 10)
+        print(name_hrac1, "si vytáhl", karta1,"\n")
+        pouzite_karty.append(karta1)
+        soucet1 = soucet1 + karta1
+        input(name_hrac2 + ": Stiskněte ENTER k vytáhnutí karty: ")
+        karta2 = random.randint(1, 10)
+        print(name_hrac2, "si vytáhl", karta2,"\n")
+        pouzite_karty.append(karta2)
+        soucet2 = soucet2 + karta2
+        print("Součet hráčů:")
+        print(name_hrac1,":", soucet1)
+        print(name_hrac2,":", soucet2,"\n")
+        while spravna_odpoved1:
+            odpoved_hrac1 = input(name_hrac1 + ": Chce si " + name_hrac1 + " líznout kartu y/n: ")
+            if odpoved_hrac1 == "y" or odpoved_hrac1 == "Y":
+                hrac1 = True
+                hrac2 = False
+                pokracovat_hra = True
+                spravna_odpoved1 = False
+                prvni_kolo = False
+            elif odpoved_hrac1 == "n" or odpoved_hrac1 == "N":
+                hrac1 = False
+                spravna_odpoved1 = False
+                pokracovat_hra = True
+            else:
+                print(name_hrac1, ": Zadali jste neplatnou volbu")
+                spravna_odpoved = True
+        if hrac1 == False:
+            while spravna_odpoved2:
+                odpoved_hrac2 = input(name_hrac2 + ": Chce si " + name_hrac2 + " líznout kartu y/n: ")
+                if odpoved_hrac2 == "y" or odpoved_hrac2 == "Y":
+                    hrac1 = False
+                    hrac2 = True
+                    pokracovat_hra = True
+                    spravna_odpoved2 = False
+                    prvni_kolo = False
+                elif odpoved_hrac2 == "n" or odpoved_hrac2 == "N":
+                    hrac1 = False
+                    hrac2 = False
+                    spravna_odpoved2 = False
+                    prvni_kolo = False
+                else:
+                    print(name_hrac2, ": Zadali jste neplatnou volbu")
+                    spravna_odpoved2 = True
     while pokracovat_hra:
         while hrac1:
-            if soucet1 <= 21:
-                print(name_hrac1,":")
-                input("Stisknete ENTER k vytahnuti karty: ")
+            if soucet1 <= 21 and soucet2 <= 21:
                 karta1 = random.randint(1, 10)
                 if karta1 not in blacklist:
                     pouzite_karty.append(karta1)
@@ -35,47 +87,48 @@ while pokracovat_program:
                                 opakovani = 1
                         last = number
                     soucet1 = soucet1 + karta1
-                    print(name_hrac1, "si vytahl", karta1,"\n")
-                    print("Soucet hracu:")
+                    print(name_hrac1, "si vytáhl", karta1,"\n")
+                    print("Součet hráčů:")
                     print(name_hrac1,":", soucet1)
                     print(name_hrac2,":", soucet2,"\n")
-                    odpoved_hrac2 = input("Chce si " + name_hrac2 + " liznout kartu y/n: ")
-                    spatnaodpoved1 = True
-                    if odpoved_hrac2 == "y" or odpoved_hrac2 == "Y":
-                        hrac2 = True
-                        hrac1 = False
-                    elif odpoved_hrac2 == "n" or odpoved_hrac2 == "N":
-                        hrac2 = False
+                    if soucet1 <= 21 and soucet2 <= 21:
+                        while spravna_odpoved3:
+                            odpoved_hrac2 = input(name_hrac2 + ": Chce si " + name_hrac2 + " líznout kartu y/n: ")
+                            if odpoved_hrac2 == "y" or odpoved_hrac2 == "Y":
+                                hrac2 = True
+                                hrac1 = False
+                                spravna_odpoved3 = False
+                            elif odpoved_hrac2 == "n" or odpoved_hrac2 == "N":
+                                hrac2 = False
+                                spravna_odpoved3 = False
+                            else:
+                                print(name_hrac2, ": Zadali jste neplatnou volbu")
+                                spravna_odpoved3 = True
+                        spravna_odpoved3 = True
+                        if hrac2 == False:
+                            while spravna_odpoved4:
+                                odpoved_hrac1 = input(name_hrac1 + ": Chce si " + name_hrac1 + " líznout kartu y/n: ")
+                                if odpoved_hrac1 == "y" or odpoved_hrac1 == "Y":
+                                    hrac1 = True
+                                    hrac2 = False
+                                    spravna_odpoved4 = False
+                                elif odpoved_hrac1 == "n" or odpoved_hrac1 == "N":
+                                    hrac1 = False
+                                    hrac2 = False
+                                    spravna_odpoved4 = False
+                                else:
+                                    print(name_hrac1, ": Zadali jste neplatnou volbu")
+                                    spravna_odpoved4 = True
+                            spravna_odpoved4 = True
                     else:
-                        print("Zadali jste neplatnou volbu")
-                    if hrac2 == False:
-                        odpoved_hrac1 = input("Chce si " + name_hrac1 + " liznout kartu y/n: ")
-                        if odpoved_hrac1 == "y" or odpoved_hrac1 == "Y":
-                            hrac1 = True
-                            hrac2 = False
-                        elif odpoved_hrac1 == "n" or odpoved_hrac1 == "N":
-                            hrac1 = False
-                            hrac2 = False
-                        else:
-                            print("Zadali jste neplatnou volbu")
-                else:
-                    print(name_hrac1, "prekrocil 21")
-                    hrac1 = False
-                    hrac2 = False
-                    if soucet2 < 21:
-                        odpoved_hrac2 = input("Chce si " + name_hrac2 + " liznout kartu y/n: ")
-                        if odpoved_hrac2 == "y" or odpoved_hrac2 == "Y":
-                            hrac1 = False
-                            hrac2 = True
-                        elif odpoved_hrac2 == "n" or odpoved_hrac2 == "N":
-                            hrac1 = False
-                            hrac2 = False
-                        else:
-                            print("Zadali jste neplatnou volbu")
+                        hrac1 = False
+                        hrac2 = False
+            else:
+                print(name_hrac1, "překročil 21")
+                hrac1 = False
+                hrac2 = False
         while hrac2:
-            if soucet2 <= 21:
-                print(name_hrac2,":")
-                input("Stisknete ENTER k vytahnuti karty: ")
+            if soucet2 <= 21 and soucet1 <= 21:
                 karta2 = random.randint(1, 10)
                 if karta2 not in blacklist:
                     pouzite_karty.append(karta2)
@@ -91,71 +144,75 @@ while pokracovat_program:
                                 opakovani = 1
                         last = number
                     soucet2 = karta2 + soucet2
-                    print(name_hrac2, "si vytahl", karta2,"\n")
-                    print("Soucet hracu:")
+                    print(name_hrac2, "si vytáhl", karta2,"\n")
+                    print("Součet hráčů:")
                     print(name_hrac1,":", soucet1)
-                    print(name_hrac2,":", soucet2)
-                    odpoved_hrac1 = input("Chce si " + name_hrac1 + " liznout kartu y/n: ")
-                    spatna_odpoved2 = True
-                    if odpoved_hrac1 == "y" or odpoved_hrac1 == "Y":
-                        hrac1 = True
-                        hrac2 = False
-                    elif odpoved_hrac1 == "n" or odpoved_hrac1 == "N":
-                        hrac1 = False
+                    print(name_hrac2,":", soucet2,"\n")
+                    if soucet1 <= 21 and soucet2 <= 21:
+                        while spravna_odpoved5:
+                            odpoved_hrac1 = input(name_hrac1 + ": Chce si " + name_hrac1 + " líznout kartu y/n: ")
+                            if odpoved_hrac1 == "y" or odpoved_hrac1 == "Y":
+                                hrac1 = True
+                                hrac2 = False
+                                spravna_odpoved5 = False
+                            elif odpoved_hrac1 == "n" or odpoved_hrac1 == "N":
+                                hrac1 = False
+                                spravna_odpoved5 = False
+                            else:
+                                print(name_hrac1, ": Zadali jste neplatnou volbu")
+                                spravna_odpoved5 = True
+                        spravna_odpoved5 = True
+                        if hrac1 == False:
+                            while spravna_odpoved6:
+                                odpoved_hrac2 = input(name_hrac2 + ": Chce si " + name_hrac2 + " líznout kartu y/n: ")
+                                if odpoved_hrac2 == "y" or odpoved_hrac2 == "Y":
+                                    hrac1 = False
+                                    hrac2 = True
+                                    spravna_odpoved6 = False
+                                elif odpoved_hrac2 == "n" or odpoved_hrac2 == "N":
+                                    hrac1 = False
+                                    hrac2 = False
+                                    spravna_odpoved6 = False
+                                else:
+                                    print(name_hrac2, ": Zadali jste neplatnou volbu") 
+                                    spravna_odpoved6 = True
+                            spravna_odpoved6 = True
                     else:
-                        print("Zadali jste neplatnou volbu")
-                    if hrac1 == False:
-                        odpoved_hrac2 = input("Chce si " + name_hrac2 + " liznout kartu y/n: ")
-                        if odpoved_hrac2 == "y" or odpoved_hrac2 == "Y":
-                            hrac1 = False
-                            hrac2 = True
-                        elif odpoved_hrac2 == "n" or odpoved_hrac2 == "N":
-                            hrac1 = False
-                            hrac2 = False
-                        else:
-                            print("Zadali jste neplatnou volbu") 
-                elif soucet2 > 21 :
-                    print(name_hrac2, "prekrocil 21") 
-                    hrac2 = False
-                    hrac1 = False
-                    if soucet1 < 21:
-                        odpoved_hrac1 = input("Chce si " + name_hrac1 + " liznout kartu y/n: ")
-                        if odpoved_hrac1 == "y" or odpoved_hrac1 == "Y":
-                            hrac1 = True
-                            hrac2 = False
-                        elif odpoved_hrac1 == "n" or odpoved_hrac1 == "N":
-                            hrac1 = False
-                            hrac2 = False
-                        else:
-                            print("Zadali jste neplatnou volbu")
+                        hrac1 = False
+                        hrac2 = False   
         if (hrac1 == False and hrac2 == False):              #zacatek vyhodnoceni
             if (soucet1 <= 21 and soucet2 <= 21):
                 if soucet1 > soucet2:
-                    print(name_hrac1, "vyhral")
+                    print(name_hrac1, "vyhrál")
                 elif soucet1 < soucet2:
-                    print(name_hrac2, "vyhral")
+                    print(name_hrac2, "vyhrál")
                 elif soucet1 == soucet2:
-                    print("Nastala remiza")
+                    print("Nastala remíza")
                 else:
-                    print("Nekde nastala chyba") 
+                    print("Někde nastala chyba") 
             elif (soucet1 <= 21 and soucet2 > 21):
-                print(name_hrac1, "vyhral")
+                print(name_hrac1, "vyhrál")
             elif (soucet2 <= 21 and soucet1 > 21):
-                print(name_hrac2, "vyhral")
+                print(name_hrac2, "vyhrál")
             elif (soucet1 == 21 and soucet2 == 21):
-                print("Remiza")
+                print("Remíza")
             elif (soucet1 > 21 and soucet2 > 21):
                 if soucet1 < soucet2:
-                    print("Vyhral", name_hrac1)
+                    print("Vyhrál", name_hrac1)
                 elif soucet2 < soucet1:
-                    print("Vyhral", name_hrac2)
+                    print("Vyhrál", name_hrac2)
                 else:
-                    print("Nekde nastala chyba")
-            odpoved_program = input("Chcete dat novou hru y/n: ")
-            if odpoved_program == "y" or odpoved_program == "Y":
-                pokracovat_program = True
-                pokracovat_hra = False
-            elif odpoved_program == "n" or odpoved_program == "N":
-                pokracovat_program = False
-                pokracovat_hra = False                           #konec vyhodnoceni                 
-
+                    print("Někde nastala chyba")
+            while spravna_odpoved7:
+                odpoved_program = input("Chcete dát novou hru y/n: ")
+                if odpoved_program == "y" or odpoved_program == "Y":
+                    pokracovat_program = True
+                    pokracovat_hra = False
+                    spravna_odpoved7 = False
+                elif odpoved_program == "n" or odpoved_program == "N":
+                    pokracovat_program = False
+                    spravna_odpoved7 = False
+                else:
+                    print("Zadali jste neplatnou volbu")
+                    spravna_odpoved7 = True
+                    pokracovat_hra = False                           #konec vyhodnoceni 
